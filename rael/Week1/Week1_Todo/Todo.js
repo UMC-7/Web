f54f6255973f4bdfc44cf09d8f1d6929a99a5d50
rael <ekosemily@gmail.com>
2024-10-06 16:02:43 +0900

    FEAT: 1주차 미션 추가

--- rael/Week1/Week1_Todo/Todo.js
+++ rael/Week1/Week1_Todo/Todo.js
@@ -9,8 +9,44 @@ todoInput.addEventListener("keypress", (e)  => {
 
 // 2. addTodo
 function addTodo(){
-    var newTodo = document.getElementById("input").value;
+    var input = document.getElementById("input");
+    var newTodo = input.value.trim();
 
-    document.getElementById("todoTask").innerText = newTodo;
-    d
+    if(newTodo !== "") {
+        var listItem = document.createElement("li");
+        listItem.textContent = newTodo;
+        // innerText 는 사용자에게 보여지는 텍스트만 가져옴
+        // textContent는 숨겨진 텍스트까지 포함해서 텍스트값을 모두 다 가져옴
+
+        // *입력한 투두 해야 할 일에 추가하기
+        var todoTask = document.getElementById("todoTask");
+        todoTask.appendChild(listItem);
+        input.value = '';
+        // appendChild() 부모노드에 자식노드 추가
+        // append() 부모노드에 자식노드를 추가할 수 있을 뿐만 아니라, text도 추가 가능
+
+        // *완료 버튼 만들기
+        var completeButton = document.createElement("button");
+        completeButton.textContent = "완료";
+        listItem.appendChild(completeButton);
+
+        // *완료 버튼을 눌렀을 때
+        completeButton.addEventListener("click", function() {
+            var completeTask = document.getElementById("todoComplete");
+            completeTask.appendChild(listItem);
+            completeButton.remove();
+
+            listItem.appendChild(deleteButton);
+        })
+
+        // *삭제 버튼 만들기
+        var deleteButton = document.createElement("button");
+        deleteButton.textContent = "삭제";
+        
+        // *삭제 버튼을 눌렀을 때
+        deleteButton.addEventListener("click", function() {
+            listItem.remove();
+        })
+    }
+    
 }
