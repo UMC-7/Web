1701207073042109c686692213ad48781562510a
rael <ekosemily@gmail.com>
2024-10-06 21:52:27 +0900

    CHORE: 2주차 미션 영화 컴포넌트 수정

---
+++ rael/Week2/Week2_Movie/src/Movie.jsx
@@ -0,0 +1,13 @@
+import './Movie.css'
+import { MoviePoster } from './components/MoviePoster'
+
+function Movie() {
+
+  return (
+    <>
+     <MoviePoster />
+    </>
+  )
+}
+
+export default Movie
