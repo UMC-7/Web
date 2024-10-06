c52b17ffefda7c074f458de86c0615b796b0ed6c
rael <ekosemily@gmail.com>
2024-10-07 00:40:25 +0900

    CHORE: 3주차 내용 추가

--- rael/Week3/Week3/src/App.jsx
+++ rael/Week3/Week3/src/App.jsx
@@ -1,13 +1,34 @@
 import './App.css'
-import CustomButton from "./components/custom-button.jsx"
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+import HomePage from './pages/home.jsx'
+import MoviesPage from './pages/movies.jsx'
+import NotFound from './pages/not-found.jsx'
+import RootLayout from './layout/root-layout.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/', 
+    element: <RootLayout/>,
+    errorElement: <NotFound/>,
+
+    children: [
+      {
+        index: true,
+        element: <HomePage/>
+      },
+      {
+        path: 'movies/:movieId',
+        element: <MoviesPage/>
+      }
+    ]
+  }
+])
 
 function App() {
 
-  return (
-    <>
-      <CustomButton />
-    </>
-  )
+  return <RouterProvider router={router} />
 }
 
 export default App
