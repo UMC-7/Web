eada88c5373fedb386fc89a1561e189dcc4d6cc4
rael <ekosemily@gmail.com>
2024-11-19 00:41:02 +0900

    FEAT: 내 정보를 불러오는 API useQuery 구현

--- rael/Week7/Week7_Movie_1_2/src/components/navbar.jsx
+++ rael/Week7/Week7_Movie_1_2/src/components/navbar.jsx
@@ -1,29 +1,57 @@
-import { useEffect, useState } from "react";
-import { Link, NavLink } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
+import { Link } from "react-router-dom";
 import styled from "styled-components";
 
 const Navbar = () => {
-    const [name, setName] = useState('');
 
-    useEffect(() => {
-        const storedName = localStorage.getItem('name');
-        if(storedName) {
-            setName(storedName);
-        }
-    }, []);
+    const fetchUserInfo = async() => {
+        const response = await axios.get("/api/user-info", {
+            headers: {
+                Authorization: `Bearer ${localStorage.getItem("accessToken")}`, 
+            }, 
+        });
+        return response.data;
+    };
+
+    const {data: userInfo, isLoading, isError} = useQuery({
+        queryFn: () => fetchUserInfo, 
+        queryKey: ['userInfo'], 
+        cacheTime: 300000,
+        staleTime: 300000,  
+        retry: false, // 에러 시 재시도 안 함
+        enabled: !!localStorage.getItem("accessToken"),
+    })
+
+    // isPending: 데이터를 불러오는 중입니다. 데이터가 로딩중일때 IsPending true.
+    // isLoading: 데이터를 불러오는 중이거나, 재시도 중일때 true가 된다.
+
+    if (isLoading) {
+        return <MovieNav>
+            <h1 style={{color:'white'}}>Loading...</h1>
+        </MovieNav>
+    }
+    
+    if (isError) {
+        return (
+            <MovieNav>
+                <LoginButton to="/login">로그인</LoginButton>
+                <SignupButton to="/signup">회원가입</SignupButton>
+            </MovieNav>
+        );
+    }
 
     const handleLogout = () => {
         localStorage.removeItem('name');
         localStorage.removeItem('accessToken');
         localStorage.removeItem('refreshToken');
-        setName('');
+        window.location.reload(); // 로그아웃 후 새로고침 해줌.
     };
 
     return (
         <MovieNav>
-            {name ? (
+            {userInfo?.name ? (
                 <>
-                    <UserName>{name}님 안녕하세요</UserName>
+                    <UserName>{userInfo.name}님 안녕하세요</UserName>
                     <LoginButton onClick={handleLogout}>로그아웃</LoginButton>
                 </>
             ) : (
